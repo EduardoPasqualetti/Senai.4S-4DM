5d95d6313da9dd2b57832511aed76fb188733f6c
Eduardo Pasqualetti <49802002810@info.local>
2024-07-29 10:35:21 -0300

    feito o crud das entidades user,client e order

---
+++ minimalAPIMongo/Controllers/OrderController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using minimalAPIMongo.Domains;
+using minimalAPIMongo.Services;
+using MongoDB.Driver;
+
+namespace minimalAPIMongo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class OrderController : ControllerBase
+    {
+        private readonly IMongoCollection<Order> _order;
+
+        public OrderController(MongoDbService mongoDbService)
+        {
+            _order = mongoDbService.GetDatabase.GetCollection<Order>("order");
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Client>>> Get()
+        {
+            try
+            {
+                var order = await _order.Find(FilterDefinition<Order>.Empty).ToListAsync();
+
+                return Ok(order);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(Order order)
+        {
+            try
+            {
+                _order.InsertOne(order);
+
+                return StatusCode(201, order);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpDelete("id")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            try
+            {
+                var filter = Builders<Order>.Filter.Eq(x => x.Id, id);
+                await _order.DeleteOneAsync(filter);
+
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpGet("id")]
+        public async Task<ActionResult<Order>> GetById(string id)
+        {
+            try
+            {
+                var order = await _order.Find(x => x.Id == id).FirstOrDefaultAsync();
+
+                return order is not null ? Ok(order) : NotFound();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+        [HttpPut]
+        public async Task<IActionResult> Put(Order order)
+        {
+            try
+            {
+                var filter = Builders<Order>.Filter.Eq(x => x.Id, order.Id);
+                await _order.ReplaceOneAsync(filter, order);
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+    }
+
+}
---
+++ minimalAPIMongo/Domains/Order.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace minimalAPIMongo.Domains
+{
+    public class Order
+    {
+        [BsonId]
+
+        [BsonElement("_id"), BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("date")]
+        public DateTime Date { get; set; }
+
+        [BsonElement("status")]
+        public string Status { get; set; }
+
+        [BsonIgnore]
+        public Client client { get; set; }
+       
+
+    }
+}
