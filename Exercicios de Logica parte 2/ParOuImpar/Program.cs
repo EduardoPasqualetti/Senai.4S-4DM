a7dbae9b35c3051383ee4ac8e525f7db45d6b2bd
Eduardo Pasqualetti <49802002810@info.local>
2024-08-12 09:26:31 -0300

    feito exercicio 1 - par ou impar

--- Exercicios de Logica parte 2/ParOuImpar/Program.cs
+++ Exercicios de Logica parte 2/ParOuImpar/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Bem vindo ao programa, informe um número inteiro:");
+int userNumber = int.Parse(Console.ReadLine()!);
+
+if (userNumber%2 == 0)
+{
+    Console.WriteLine($"o número {userNumber} é par!");
+} else{
+    Console.WriteLine($"o número {userNumber} é impar");
+}
--- Exercicios de Logica parte 2/ParOuImpar/obj/Debug/net8.0/ParOuImpar.AssemblyInfo.cs
+++ Exercicios de Logica parte 2/ParOuImpar/obj/Debug/net8.0/ParOuImpar.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ParOuImpar")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d700c45b3a07c1ca032f9bef3b2145dabba84d02")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4851a57be19274d1ad7b6e3dbbc768e1b4be175a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ParOuImpar")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ParOuImpar")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
